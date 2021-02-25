ad743c6dbb373a9d7087a95fcb08b1c6f72ec6d1
Seth Rosen <seth@hashpath.com>
2021-02-24 21:33:23 -0500

    wip

---
+++ models/viz2.sql
@@ -0,0 +1,18 @@
+{{ config(
+	type='table', 
+	dimensions=[1],
+	title='Bigfoot Sightings',
+    sortable=true,
+    searchable=true,
+    download_csv=true
+)}}
+
+SELECT sightings.state, sightings.season, ss.humidity, ss.moon_phase as moon, ss.wind_speed as wind
+FROM `hashpath-demo-data.hashpath_dataset.bigfoot_sightings_ts` sightings
+LEFT JOIN {{ ref('viz1') }} ss on ss.geohash = sightings.geohash 
+WHERE sightings.state='{{ filter('state') }}'
+
+{{ column(
+    name='moon',
+    html='<a-tag>[[ rendered ]]</a-tag>'
+)}}
